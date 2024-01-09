bfaae6b1c0fa253a43caadc46f484711028b7290
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-01-09 16:23:27 +0330

    add templates

---
+++ DsProject/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DsProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+
+
+
+    //public class Purchase
+    //{
+    //    public string? ProductName { get; set; }
+    //    public DateTime DateTime { get; set; }
+
+    //    public Decimal ProductPrice { get; set; }
+    //}
+
+
+    public partial class App : Application
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    Purchase purchase = new Purchase
+        //    {
+        //        ProductName = "amir Hossien",
+        //        DateTime = DateTime.UtcNow,
+        //        ProductPrice = 2.49m
+        //    };
+
+        //    string jsonString = JsonSerializer.Serialize(purchase);
+
+        //}
+
+
+    }
+}
---
+++ DsProject/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Text.Json;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DsProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public class Purchase
+    {
+        public string? ProductName { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Decimal ProductPrice { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+
+        bool running = false;
+
+
+        Purchase purchase = new Purchase
+        {
+            ProductName = "amir Hossien",
+            DateTime = DateTime.UtcNow,
+            ProductPrice = 2.49m
+        };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+    }
+}
