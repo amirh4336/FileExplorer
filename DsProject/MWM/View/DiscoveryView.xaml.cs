f7491a576d6f2dbb123af0ab4748bba83e5f792d
Farid_mr <itmirengineering@gmail.com>
2024-01-21 19:23:36 +0330

    hey windows explorer!

--- DsProject/MWM/View/DiscoveryView.xaml.cs
+++ DsProject/MWM/View/DiscoveryView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,56 @@ namespace DsProject.MWM.View
     /// </summary>
     public partial class DiscoveryView : UserControl
     {
+
+        private object dummyNode = null;
+
         public DiscoveryView()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Window_Loaded);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                foldersItem.Items.Add(item);
+            }
+        }
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            // Your code here. For example:
+            TreeViewItem selectedItem = (TreeViewItem)e.NewValue;
+            // Do something with selectedItem
+        }
+
+        private void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) { }
+            }
         }
     }
 }
